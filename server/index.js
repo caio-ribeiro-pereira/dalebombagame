449636295b70134ff2011a35e2922d26d3b9b6c8
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2015-08-26 01:06:09 -0300

    model optimization

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 Meteor.startup(function() {
   Players.remove({});
   Games.remove({});
-  Players._ensureIndex({clicks: 1});
-  Games._ensureIndex({timer: 1});
+  Players._ensureIndex({c: 1, p: 1, f: 1});
+  Games._ensureIndex({t: 1, r: 1});
   console.log("DaleBombaGame server running!");
 });
