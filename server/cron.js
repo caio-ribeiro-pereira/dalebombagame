f5808f241e6bb3771b81d381d89cc52ee30f35f5
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2015-09-07 21:37:30 -0300

    added Meteor.startup encapsulation

--- server/cron.js
+++ server/cron.js
@@ -1,30 +1,32 @@
-SyncedCron.add({
-  name: "Game Play",
-  schedule: function(parser) {
-    return parser.text("every 1 seconds");
-  },
-  job: function() {
-    if (Games.timeWaiting()) {
-      Games.refreshWait();
-    } else {
-      var gameID = Games.currentID();
-      if (Players.hasMoreThanOne(gameID)) {
-        if (Games.isRunning()) {
-          if (Games.timeElapsed()) {
-            Games.refreshGame();
-          } else {
-            Players.winner();
-            Players.finish();
-            Games.stopGame();
-          } 
-        } else {
-          Players.start();
-          Games.run();
-        }
+Meteor.startup(function() {
+  SyncedCron.add({
+    name: "Game Play",
+    schedule: function(parser) {
+      return parser.text("every 1 seconds");
+    },
+    job: function() {
+      if (Games.timeWaiting()) {
+        Games.refreshWait();
       } else {
-        Players.clear();
-        Games.stopGame();
+        var gameID = Games.currentID();
+        if (Players.hasMoreThanOne(gameID)) {
+          if (Games.isRunning()) {
+            if (Games.timeElapsed()) {
+              Games.refreshGame();
+            } else {
+              Players.winner();
+              Players.finish();
+              Games.stopGame();
+            } 
+          } else {
+            Players.start();
+            Games.run();
+          }
+        } else {
+          Players.clear();
+          Games.stopGame();
+        }
       }
     }
-  }
+  });
 });
--- server/publications.js
+++ server/publications.js
@@ -1,11 +1,13 @@
-Meteor.publish("player", function(_id){
-  return Players.find({_id: _id});
-});
+Meteor.startup(function() {
+  Meteor.publish("player", function(_id){
+    return Players.find({_id: _id});
+  });
 
-Meteor.publish("players", function(){
-  return Players.find({});
-});
+  Meteor.publish("players", function(){
+    return Players.find({});
+  });
 
-Meteor.publish("games", function() {
-  return Games.findCurrentGame();
+  Meteor.publish("games", function() {
+    return Games.findCurrentGame();
+  });
 });
